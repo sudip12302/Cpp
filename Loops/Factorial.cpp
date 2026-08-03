feae17b718676666c425aeac3e2820ac2763c10a
Sudip Bastola <sudipbastola65@gmail.com>
2026-08-03 20:49:57 +0545

    Logics using loops

---
+++ Loops/Factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, factorial = 1;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; ++i) {
+        factorial *= i; 
+    }
+
+    cout << "Factorial of " << n << " is: " << factorial << endl;
+    return 0;
+}
