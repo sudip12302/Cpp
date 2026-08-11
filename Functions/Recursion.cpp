dfdd940e8f1984e457b20491e73ce040b3e3d1e8
Sudip Bastola <sudipbastola65@gmail.com>
2026-08-11 22:37:28 +0545

    Recursive Function in C++

---
+++ Functions/Recursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//
+bool isEven(int n) {
+    if (n == 0) {
+        return true;
+    }
+    return !isEven(n - 1);
+}
+
+int main() {
+    int num = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << num << " is " << (isEven(num) ? "even" : "odd") << endl;
+    return 0;
+}
