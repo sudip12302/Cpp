7f2817c6b35a60bf8cf5f8e9619839ec1f105c91
Sudip Bastola <sudipbastola65@gmail.com>
2026-08-21 20:00:29 +0545

    Minimum and maximum values in array

---
+++ Arrays/Finding_min_max.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int main() {
+    int size;
+    cout << "Enter the number of elements you want to compare:  ";
+    cin >> size;
+
+    int* arr = new int[size];
+    cout << "Enter " << size << " numbers: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    int minValue = arr[0];
+    int maxValue = arr[0];
+git
+    for (int i = 1; i < size; i++) {
+        minValue = min(minValue, arr[i]);
+        maxValue = max(maxValue, arr[i]);
+    }
+
+    cout << "Minimum value: " << minValue << endl;
+    cout << "Maximum value: " << maxValue << endl;
+
+    return 0;
+}
