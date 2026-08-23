e88deed24de4bc6ac464babb6ebc608e88cdb6c0
Sudip Bastola <sudipbastola65@gmail.com>
2026-08-23 22:26:41 +0545

    Reversing an array

---
+++ Arrays/Reversing.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Reverse the array
+    for (int i = 0; i < n / 2; i++)
+    {
+        swap(arr[i], arr[n - i - 1]);
+    }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
