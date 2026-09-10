f4aa863c84e12324290cf660a073591cb7fc5e80
Sudip Bastola <sudipbastola65@gmail.com>
2026-09-10 21:30:00 +0545

    Polymorphism

---
+++ OOP/Polymorphism.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal makes a sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Dog barks" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void sound() override {
+        cout << "Cat meows" << endl;
+    }
+};
+
+int main() {
+    Dog dog;
+    Cat cat;
+
+    Animal* animal;
+
+    animal = &dog;
+    animal->sound();
+
+    animal = &cat;
+    animal->sound();
+
+    return 0;
+}
